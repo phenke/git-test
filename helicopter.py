6b6da2de773ee6b9b8939d6394086af811f56915
Laundrybox <laundry1@gmail.com>
2016-01-05 10:36:30 -0800

    Added sweet helicopter text adventure

---
+++ helicopter.py
@@ -0,0 +1,20 @@
+# helicopter.py
+
+import time
+
+print("You are in a helicopter\n")
+time.sleep(1)
+print("*Thwump Thwump Thwump*\n")
+time.sleep(0.8)
+print("*Thwump Thwump Thwump*\n")
+time.sleep(0.8)
+print("The pilot asks you, \"Where you headed?\"\n")
+
+destination = input()
+
+print()
+time.sleep(0.5)
+print("The pilot considers your request...\n")
+time.sleep(2)
+print("Finally he responds, \"Wrong again, Grandma!\"")
+
